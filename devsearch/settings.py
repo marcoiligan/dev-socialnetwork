6a74d8673780d96603656fc29bbc670d3e80f0f0
marco <miligan17@gmail.com>
2022-08-03 08:29:48 +0800

    fixed sensitive data

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -165,8 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mciligan1997@gmail.com'
-EMAIL_HOST_PASSWORD = 'azyccepytitscitg'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
@@ -192,9 +192,9 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_QUERYSTRING_AUTH = False
 
-AWS_ACCESS_KEY_ID = 'AKIAVBFFGUKP3VEIOF4S'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'devsearch-social-network'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 if os.getcwd() == '/app':
     DEBUG = False
