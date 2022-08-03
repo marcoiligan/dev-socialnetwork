f9fb057c43dad6f5afe81c69746e72fd498d123e
marco <miligan17@gmail.com>
2022-08-03 17:09:44 +0800

    fixed alignment of text

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -196,5 +196,6 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
+
 if os.getcwd() == '/app':
     DEBUG = False
